217f1dc0fa64c67a7711342a3413ea478534675f
mithridatem <mathieumithridate@adrar-formation.com>
2025-11-17 13:27:46 +0100

    feat : Model category + request insert + select all

--- App/Model/Category.php
+++ App/Model/Category.php
@@ -7,8 +7,8 @@ use App\Database\Mysql;
 class Category
 {
     //Attributs
-    private int $id;
-    private string $name;
+    private ?int $id;
+    private ?string $name;
     private \PDO $connect;
 
     //Constructeur
@@ -22,11 +22,65 @@ class Category
     }
 
     //Getters et Setters
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
     //Méthodes
     //Ajouter une category
-
+    public function saveCategory(Category $category): void
+    {
+        try {
+            //Requête SQL
+            $sql = "INSERT INTO category(`name`) VALUE(?)";
+            //péparation
+            $req = $this->connect->prepare($sql);
+            //Assignation du paramètre
+            $req->bindValue(1, $category->getName(), \PDO::PARAM_STR);
+            //Exécution de la requête
+            $req->execute();
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
     //Afficher une category (par son id)
-
+    public function findAllCategoryById(int $id): array
+    {
+        return [];
+    }
+    
     //Afficher toutes les categories
+    public function findAllCategories():array
+    {
+        try {
+            //Requête SQL
+            $sql = "SELECT c.id, c.name FROM category AS c";
+            //péparation
+            $req = $this->connect->prepare($sql);
+            //Exécution de la requête
+            $req->execute();
+            //Fetch
+            $categories = $req->fetchAll(\PDO::FETCH_ASSOC);
+            
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+        //Retour de la liste des categories
+        return $categories;
+    }
 }
--- template/template_home.php
+++ template/template_home.php
@@ -1,10 +1,18 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <title><?= $title ?></title>
 </head>
+
 <body>
-    <h1>Bienvenue sur notre super site</h1>
+    <main class="container">
+        <h1>Bienvenue sur notre super site</h1>
+    </main>
 </body>
+
 </html>
